310e6e6573afdba17900bb4b680870599d0c1cae
kjinan <2008kongxiangsheng@163.com>
2021-05-21 08:57:32 +0800

    'update'

--- config/reset_ignore_words.py
+++ config/reset_ignore_words.py
@@ -1,4 +1,3 @@
-
 f = open('ignore_words.txt', "r+", encoding='utf-8')
 
 # 读取内容
@@ -10,7 +9,7 @@ f.truncate()
 f.flush()
 
 # 将内容排重、排序后重新写入
-new_contents = list(set(contents))
+new_contents = [item.replace(' ', '') for item in set(contents)]
 new_contents.sort()
 f.writelines(new_contents)
 f.flush()
